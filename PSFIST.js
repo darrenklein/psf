f6f9753cdce639530d31273ae0045b1b511dbbe0
Darren Klein <darren.a.klein@gmail.com>
2016-04-10 16:43:17 -0400

    Commit success! Working on one more validation field - file size.  Current server restrictions only allow 48Mb of attachments, so created totalFileSize variable to track uploads, keep total under 48Mb

--- PSFIST.js
+++ PSFIST.js
@@ -42,6 +42,14 @@ $(document).ready(function(){
     var speciesList = "<option disabled selected>Select species</option><option value='unknown'>unknown</option><option value='other'>other (describe in 'Notes')</option><option value='bat sp.'>bat sp.</option><option value='flycatcher sp.'>flycatcher sp.</option><option value='gull sp.'>gull sp.</option><option value='kinglet sp.'>kinglet sp.</option><option value='owl sp.'>owl sp.</option><option value='sparrow sp.'>sparrow sp.</option><option value='thrush sp.'>thrush sp.</option><option value='warbler sp.'>warbler sp.</option><option value='woodpecker sp.'>woodpecker sp.</option><option value='wren sp.'>wren sp.</option><option value='Alder Flycatcher'>Alder Flycatcher</option><option value='American Black Duck'>American Black Duck</option><option value='American Coot'>American Coot</option><option value='American Goldfinch'>American Goldfinch</option><option value='American Kestrel'>American Kestrel</option><option value='American Redstart'>American Redstart</option><option value='American Robin'>American Robin</option><option value='American Woodcock'>American Woodcock</option><option value='Baltimore Oriole'>Baltimore Oriole</option><option value='Barn Swallow'>Barn Swallow</option><option value='Bay-Breasted Warbler'>Bay-Breasted Warbler</option><option value='Belted Kingfisher'>Belted Kingfisher</option><option value='Black-and-white Warbler'>Black-and-white Warbler</option><option value='Black-billed Cuckoo'>Black-billed Cuckoo</option><option value='Black-capped Chickadee'>Black-capped Chickadee</option><option value='Blackpoll Warbler'>Blackpoll Warbler</option><option value='Black-throated Blue Warbler'>Black-throated Blue Warbler</option><option value='Black-throated Green Warbler'>Black-throated Green Warbler</option><option value='Blue Jay'>Blue Jay</option><option value='Blue-headed Vireo'>Blue-headed Vireo</option><option value='Blue-winged Warbler'>Blue-winged Warbler</option><option value='Brown Creeper'>Brown Creeper</option><option value='Brown Thrasher'>Brown Thrasher</option><option value='Brown-headed Cowbird'>Brown-headed Cowbird</option><option value='Canada Warbler'>Canada Warbler</option><option value='Cape May Warbler'>Cape May Warbler</option><option value='Cedar Waxwing'>Cedar Waxwing</option><option value='Chestnut-sided Warbler'>Chestnut-sided Warbler</option><option value='Chipping Sparrow'>Chipping Sparrow</option><option value='Chuck-wills-widow'>Chuck-will's-widow</option><option value='Clapper Rail'>Clapper Rail</option><option value='Common Grackle'>Common Grackle</option><option value='Common Yellowthroat'>Common Yellowthroat</option><option value='Connecticut Warbler'>Connecticut Warbler</option><option value='Dark-eyed Junco'>Dark-eyed Junco</option><option value='Downy Woodpecker'>Downy Woodpecker</option><option value='Eastern Bluebird'>Eastern Bluebird</option><option value='Eastern Kingbird'>Eastern Kingbird</option><option value='Eastern Phoebe'>Eastern Phoebe</option><option value='Eastern Towhee'>Eastern Towhee</option><option value='Eastern Wood-pewee'>Eastern Wood-pewee</option><option value='European Starling'>European Starling</option><option value='Field Sparrow'>Field Sparrow</option><option value='Fox Sparrow'>Fox Sparrow</option><option value='Golden-crowned Kinglet'>Golden-crowned Kinglet</option><option value='Gray Catbird'>Gray Catbird</option><option value='Gray-cheeked Thrush'>Gray-cheeked Thrush</option><option value='Great Crested Flycatcher'>Great Crested Flycatcher</option><option value='Hermit Thrush'>Hermit Thrush</option><option value='Hooded Warbler'>Hooded Warbler</option><option value='House Finch'>House Finch</option><option value='House Sparrow'>House Sparrow</option><option value='House Wren'>House Wren</option><option value='Indigo Bunting'>Indigo Bunting</option><option value='Kentucky Warbler'>Kentucky Warbler</option><option value='Lazuli Bunting'>Lazuli Bunting</option><option value='Least Flycatcher'>Least Flycatcher</option><option value='Lincolns Sparrow'>Lincoln's Sparrow</option><option value='Louisiana Waterthrush'>Louisiana Waterthrush</option><option value='Magnolia Warbler'>Magnolia Warbler</option><option value='Marsh Wren'>Marsh Wren</option><option value='Mourning Dove'>Mourning Dove</option><option value='Mourning Warbler'>Mourning Warbler</option><option value='Northern Cardinal'>Northern Cardinal</option><option value='Northern Flicker'>Northern Flicker</option><option value='Northern Mockingbird'>Northern Mockingbird</option><option value='Northern Parula'>Northern Parula</option><option value='Northern Waterthrush'>Northern Waterthrush</option><option value='Olive-sided Flycatcher'>Olive-sided Flycatcher</option><option value='Orchard Oriole'>Orchard Oriole</option><option value='Osprey'>Osprey</option><option value='Ovenbird'>Ovenbird</option><option value='Palm Warbler'>Palm Warbler</option><option value='Peregrine Falcon'>Peregrine Falcon</option><option value='Pine Warbler'>Pine Warbler</option><option value='Prairie Warbler'>Prairie Warbler</option><option value='Prothonotary Warbler'>Prothonotary Warbler</option><option value='Purple Finch'>Purple Finch</option><option value='Red-headed Woodpecker'>Red-headed Woodpecker</option><option value='Red-bellied Woodpecker'>Red-bellied Woodpecker</option><option value='Red-breasted Nuthatch'>Red-breasted Nuthatch</option><option value='Red-eyed Vireo'>Red-eyed Vireo</option><option value='Red-tailed Hawk'>Red-tailed Hawk</option><option value='Rock Pigeon'>Rock Pigeon</option><option value='Rose-breasted Grosbeak'>Rose-breasted Grosbeak</option><option value='Ruby-crowned Kinglet'>Ruby-crowned Kinglet</option><option value='Ruby-throated Hummingbird'>Ruby-throated Hummingbird</option><option value='Savannah Sparrow'>Savannah Sparrow</option><option value='Scarlet Tanager'>Scarlet Tanager</option><option value='Seaside Sparrow'>Seaside Sparrow</option><option value='Song Sparrow'>Song Sparrow</option><option value='Sora'>Sora</option><option value='Swainsons Thrush'>Swainson's Thrush</option><option value='Swamp Sparrow'>Swamp Sparrow</option><option value='Tennessee Warbler'>Tennessee Warbler</option><option value='Tufted Titmouse'>Tufted Titmouse</option><option value='Veery'>Veery</option><option value='Virginia Rail'>Virginia Rail</option><option value='White-breasted Nuthatch'>White-breasted Nuthatch</option><option value='White-crowned Sparrow'>White-crowned Sparrow</option><option value='White-eyed Vireo'>White-eyed Vireo</option><option value='White-throated Sparrow'>White-throated Sparrow</option><option value='Willow Flycatcher'>Willow Flycatcher</option><option value='Wilsons Warbler'>Wilson's Warbler</option><option value='Winter Wren'>Winter Wren</option><option value='Wood Thrush'>Wood Thrush</option><option value='Worm-eating Warbler'>Worm-eating Warbler</option><option value='Yellow Warbler'>Yellow Warbler</option><option value='Yellow-bellied Flycatcher'>Yellow-bellied Flycatcher</option><option value='Yellow-bellied Sapsucker'>Yellow-bellied Sapsucker</option><option value='Yellow-billed Cuckoo'>Yellow-billed Cuckoo</option><option value='Yellow-breasted Chat'>Yellow-breasted Chat</option><option value='Yellow-rumped Warbler'>Yellow-rumped Warbler</option><option value='Yellow-throated Vireo'>Yellow-throated Vireo</option>";
     
     var actionList = "<option disabled selected>Select action</option><option value='collected'>Collected</option><option value='no_change'>No change</option><option value='captured_and_released'>Captured and released</option><option value='transported_to_rehabber'>Transported to rehabber</option><option value='other'>Other (Describe in 'Notes')</option>";
+    
+    
+    
+    
+    
+    //STORE CUMULATIVE SIZE OF FILE ATTACHMENTS FOR FORM VALIDATION - CURRENT SERVER CONSTRAINTS LIMIT TOTAL ATTACHMENTS TO 48MB - SEE FUNCTION ON CHANGE EVENT FOR FILE ATTACHMENTS
+    var totalFileSize = 0;
+    
 
     
     //CREATES THE BASIC SITE FIELDS - NO BIRDS FOUND AT SITE
@@ -74,8 +82,15 @@ $(document).ready(function(){
                         siteImage = $("<input/>", {
                                     type: 'file',
                                     id: 'siteimage'+siteNumber+'',
-                                    name: 'siteimage'+siteNumber+''
-                                    });
+                                    name: 'siteimage'+siteNumber+'',
+                                    on: {
+                                        change: function(){
+                                            //TICKS UP THE TOTAL FILE SIZE VAR, FOR FORM VALIDATION
+                                            totalFileSize += this.files[0].size;
+                                            alert(totalFileSize);
+                                        }
+                                    }
+                                });
         
                         siteImageLabel = $("<label/>", {
                                     text: 'Attach an image',
@@ -88,8 +103,12 @@ $(document).ready(function(){
                                 id: 'clear'+siteNumber+'',
                                 on: {
                                     click: function(){
-                                        file = $('#siteimage'+siteNumber+'');
-                                        file.replaceWith(file = file.clone(true));
+                                        file = $('#siteimage'+siteNumber+'')[0];
+                                        totalFileSize -= file.files[0].size;
+                                        alert(totalFileSize);
+                                        
+                                        fileInput = $('#siteimage'+siteNumber+'');
+                                        fileInput.replaceWith(fileInput = fileInput.clone(true));
                                     }  
                                 }
                             });
@@ -281,7 +300,14 @@ $(document).ready(function(){
                     image = $("<input/>", {
                             type: 'file',
                             id: 'image'+siteNumber+''+x+'',
-                            name: 'image'+siteNumber+'['+x+']'
+                            name: 'image'+siteNumber+'['+x+']',
+                            on: {
+                                change: function(){
+                                    //TICKS UP THE TOTAL FILE SIZE VAR, FOR FORM VALIDATION
+                                    totalFileSize += this.files[0].size;
+                                    alert(totalFileSize);
+                                    }
+                                }
                             });
         
                     imageLabel = $("<label/>", {
@@ -295,9 +321,13 @@ $(document).ready(function(){
                             class: 'clear',
                             id: 'clear'+siteNumber+''+x+'',
                             on: {
-                                    click: function(){
-                                        file = $('#image'+siteNumber+''+x+'');
-                                        file.replaceWith(file = file.clone(true));
+                                    click: function(){        
+                                        file = $('#image'+siteNumber+''+x+'')[0];
+                                        totalFileSize -= file.files[0].size;
+                                        alert(totalFileSize);
+                                
+                                        fileInput = $('#image'+siteNumber+''+x+'');
+                                        fileInput.replaceWith(fileInput = fileInput.clone(true));
                                     }  
                                 }
                             });
@@ -320,7 +350,7 @@ $(document).ready(function(){
         
     };
     
-    
+
 
     
 
